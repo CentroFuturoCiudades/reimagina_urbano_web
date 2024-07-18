eb307c4963500669a33b37d6f2a18f918590d738
urielsu96 <uriel.s@tec.mx>
2024-07-17 18:09:11 -0600

    Refactorizar uso base map

--- src/App.tsx
+++ src/App.tsx
@@ -1,142 +1,157 @@
 import { Box, Radio, RadioGroup, Stack } from "@chakra-ui/react";
-import { useEffect, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 import "@chatscope/chat-ui-kit-styles/dist/default/styles.min.css";
 import axios from "axios";
+import * as turf from "@turf/turf";
 
 import { Custom3DMap } from "./Custom3DMap";
 import { CustomMap } from "./CustomMap";
 import { API_URL } from "./constants";
 import "./App.css";
-import { Chat, LotSidebar, ConfigurationToolbar, MainSidebar, BaseMap } from "./components";
+import {
+    Chat,
+    LotSidebar,
+    ConfigurationToolbar,
+    MainSidebar,
+    BaseMap,
+} from "./components";
 import { Icon, IconButton } from "@chakra-ui/react";
 import { MdAdd, MdOutlineMotionPhotosOff } from "react-icons/md";
 import React from "react";
 import { Configuration, GenericObject } from "./types";
 import { LensMap } from "./LensMap";
 import { useFetchGeo } from "./utils";
+import { debounce } from "lodash";
+import { GeoJsonLayer } from "@deck.gl/layers";
+import { PickInfo } from "deck.gl";
+import { RootState } from "./app/store";
+import { useSelector } from "react-redux";
+import { setSelectedLots } from "./features/selectedLots/selectedLotsSlice";
 //import Legend from "./Legend";
 
 function App() {
-  const [data, setData] = useState<any[]>([]);
-  const [selectedLots, setSelectedLots] = useState<string[]>([]);
-  const [aggregatedInfo, setAggregatedInfo] = useState<GenericObject>();
-  const [minutesData, setMinutesData] = useState<any[]>();
-  const [configuration, setConfiguration] = useState<GenericObject>({
-    condition: undefined,
-    metric: "POBTOT",
-    isSatellite: false,
-    visible: {
-      parking: true,
-      building: true,
-      potential_building: true,
-      park: true,
-      green: false,
-      equipment: true,
-    },
-    accessibility_info: {
-      // Initialize this with default values for proximity settings
-      proximity_small_park: 2,
-      proximity_salud: 2,
-      proximity_educacion: 1,
-      proximity_servicios: 5,
-      proximity_supermercado: 1,
-    },
-  });
-  // console.log(selectedLots); // igual a selected Ids
-  const [coords, setCoords] = useState();
-  const [parques, setParques] = useState<Configuration>({
-    activated: true,
-    value: 0,
-  });
-  const [salud, setSalud] = useState({ activated: true, value: 0 });
-  const [educacion, setEducacion] = useState({ activated: true, value: 0 });
-  const [servicios, setServicios] = useState({ activated: true, value: 0 });
-  const [supermercados, setSupermercados] = useState({
-    activated: true,
-    value: 0,
-  });
-  const project = window.location.pathname.split("/")[1];
-  const [mode, setMode] = useState("analysis");
+    const [data, setData] = useState<any[]>([]);
+    const selectedLots = useSelector(
+        (state: RootState) => state.selectedLots.selectedLots
+    );
+    const [aggregatedInfo, setAggregatedInfo] = useState<GenericObject>();
+    const [minutesData, setMinutesData] = useState<any[]>();
+    const [configuration, setConfiguration] = useState<GenericObject>({
+        condition: undefined,
+        metric: "POBTOT",
+        isSatellite: false,
+        visible: {
+            parking: true,
+            building: true,
+            potential_building: true,
+            park: true,
+            green: false,
+            equipment: true,
+        },
+        accessibility_info: {
+            // Initialize this with default values for proximity settings
+            proximity_small_park: 2,
+            proximity_salud: 2,
+            proximity_educacion: 1,
+            proximity_servicios: 5,
+            proximity_supermercado: 1,
+        },
+    });
+    // console.log(selectedLots); // igual a selected Ids
+    // const [coords, setCoords] = useState();
+    const [parques, setParques] = useState<Configuration>({
+        activated: true,
+        value: 0,
+    });
+    const [salud, setSalud] = useState({ activated: true, value: 0 });
+    const [educacion, setEducacion] = useState({ activated: true, value: 0 });
+    const [servicios, setServicios] = useState({ activated: true, value: 0 });
+    const [supermercados, setSupermercados] = useState({
+        activated: true,
+        value: 0,
+    });
+    const project = window.location.pathname.split("/")[1];
+    const [mode, setMode] = useState("analysis");
 
-  // if project is undefined, redirect to /primavera
-  if (project === "") {
-    window.location.href = "/primavera";
-  }
-
-  useEffect(() => {
-    async function updateProject() {
-      await axios.get(`${API_URL}/project/${project}`);
-      const coords = await axios.get(`${API_URL}/coords`);
-      setCoords(coords.data);
+    // if project is undefined, redirect to /primavera
+    if (project === "") {
+        window.location.href = "/primavera";
     }
-    updateProject();
-  }, [project]);
 
-  // console.log(aggregatedInfo)
+    // useEffect(() => {
+    //   async function updateProject() {
+    //     await axios.get(`${API_URL}/project/${project}`);
+    //     const coords = await axios.get(`${API_URL}/coords`);
+    //     setCoords(coords.data);
+    //   }
+    //   updateProject();
+    // }, [project]);
 
-  useEffect(() => {
-    // console.log(selectedLots)
+    // console.log(aggregatedInfo)
 
-    const fetchData = async () => {
-      if (selectedLots.length > 0) {
-        const response = await axios.get(
-          `${API_URL}/predios/?${selectedLots
-            .map((x) => `predio=${x}`)
-            .join("&")}`
-        );
-        setAggregatedInfo(response.data);
-      } else {
-        setAggregatedInfo(undefined);
-      }
-    };
-    fetchData();
-  }, [selectedLots, coords]);
+    // useEffect(() => {
+    //   // console.log(selectedLots)
 
-  useEffect(()=> {
-    setMinutesData( [] );
-  }, [configuration.accessibility_info])
+    //   const fetchData = async () => {
+    //     if (selectedLots.length > 0) {
+    //       const response = await axios.get(
+    //         `${API_URL}/predios/?${selectedLots
+    //           .map((x) => `predio=${x}`)
+    //           .join("&")}`
+    //       );
+    //       setAggregatedInfo(response.data);
+    //     } else {
+    //       setAggregatedInfo(undefined);
+    //     }
+    //   };
+    //   fetchData();
+    // }, [selectedLots, coords]);
 
-  useEffect(() => {
-    async function fetchData() {
-      // console.log(configuration.metric); // wasteful_ratio, la metrica que se muestra
+    useEffect(() => {
+        setMinutesData([]);
+    }, [configuration.accessibility_info]);
 
-      if( configuration.metric == "minutes" ){
+    // useEffect(() => {
+    //   async function fetchData() {
+    //     // console.log(configuration.metric); // wasteful_ratio, la metrica que se muestra
 
-        if( minutesData && minutesData.length ){
-          setData( minutesData );
-          return;
-        }
+    //     if( configuration.metric == "minutes" ){
 
-        const url = `${API_URL}/minutes`;
-        const body = {
-          accessibility_info: configuration.accessibility_info
-        }
+    //       if( minutesData && minutesData.length ){
+    //         setData( minutesData );
+    //         return;
+    //       }
 
-        axios.post(url, body)
-          .then( (response) => {
-            setMinutesData( response.data );
-            setData(response.data)
-          } )
+    //       const url = `${API_URL}/minutes`;
+    //       const body = {
+    //         accessibility_info: configuration.accessibility_info
+    //       }
 
-      } else {
-        const response = await axios.post(`${API_URL}/query`, {
-          metric: configuration.metric,
-          condition: configuration.condition,
-          accessibility_info: configuration.accessibility_info,
-        });
-        setData(response.data);
-      }
-    }
-    fetchData();
-  }, [
-    configuration.metric,
-    configuration.condition,
-    configuration.accessibility_info,
-  ]);
+    //       axios.post(url, body)
+    //         .then( (response) => {
+    //           setMinutesData( response.data );
+    //           setData(response.data)
+    //         } )
 
-  return (
-    <div style={{ width: "100dvw", height: "100dvh" }}>
-      {/* <Chat
+    //     } else {
+    //       const response = await axios.post(`${API_URL}/query`, {
+    //         metric: configuration.metric,
+    //         condition: configuration.condition,
+    //         accessibility_info: configuration.accessibility_info,
+    //       });
+    //       setData(response.data);
+    //     }
+    //   }
+    //   fetchData();
+    // }, [
+    //   configuration.metric,
+    //   configuration.condition,
+    //   configuration.accessibility_info,
+    // ]);
+
+    return (
+        <div style={{ width: "100dvw", height: "100dvh" }}>
+            {/* <Chat
         onSend={(query) =>
           setConfiguration({
             ...configuration,
@@ -145,10 +160,9 @@ function App() {
           })
         }
       /> */}
-      <MainSidebar>
-      </MainSidebar>
-      <BaseMap></BaseMap>
-      {/* {mode === "explore" ? (
+            {selectedLots.length > 0 && <MainSidebar />}
+            <BaseMap />
+            {/* {mode === "explore" ? (
         <LensMap
           aggregatedInfo={aggregatedInfo}
           data={data}
@@ -171,7 +185,7 @@ function App() {
           isSatellite={configuration.isSatellite}
         />
       )} */}
-      {/* <ConfigurationToolbar
+            {/* <ConfigurationToolbar
         configuration={configuration}
         setConfiguration={setConfiguration}
         supermercados={supermercados}
@@ -182,33 +196,33 @@ function App() {
         setSupermercados={setSupermercados}
       /> */}
 
-      {selectedLots.length > 0 && (
-        <Box
-          style={{
-            position: "absolute",
-            width: "25%",
-            height: "100dvh",
-            left: 0,
-            backgroundColor: "rgba(255, 255, 255, 0.9)",
-            zIndex: 1000,
-            overflowY: "scroll",
-            overflowX: "hidden",
-          }}
-        >
-          <LotSidebar
-            aggregatedInfo={aggregatedInfo}
-            selectedLots={selectedLots}
-            setSelectedLots={setSelectedLots}
-            parques={parques}
-            salud={salud}
-            educacion={educacion}
-            servicios={servicios}
-            supermercados={supermercados}
-          />
-        </Box>
-      )}
-    </div>
-  );
+            {selectedLots.length > 0 && (
+                <Box
+                    style={{
+                        position: "absolute",
+                        width: "25%",
+                        height: "100dvh",
+                        left: 0,
+                        backgroundColor: "rgba(255, 255, 255, 0.9)",
+                        zIndex: 1000,
+                        overflowY: "scroll",
+                        overflowX: "hidden",
+                    }}
+                >
+                    <LotSidebar
+                        aggregatedInfo={aggregatedInfo}
+                        selectedLots={selectedLots}
+                        setSelectedLots={setSelectedLots}
+                        parques={parques}
+                        salud={salud}
+                        educacion={educacion}
+                        servicios={servicios}
+                        supermercados={supermercados}
+                    />
+                </Box>
+            )}
+        </div>
+    );
 }
 
 export default App;
