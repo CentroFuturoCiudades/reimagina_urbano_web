f8d695ba92f0620e727624dce08fb933ffc7a1f3
Alejandro Gonzalez <alex.glz.v@hotmail.com>
2024-07-24 12:53:47 -0600

    Visor tabs change

--- src/content/Visor.tsx
+++ src/content/Visor.tsx
@@ -2,16 +2,19 @@ import { useState } from "react";
 import { Icon, VStack, Box, Text } from "@chakra-ui/react";
 import React from "react";
 import { BiSolidHome } from "react-icons/bi";
-import { FaBuilding, FaPerson } from "react-icons/fa6";
+import { FaBuilding, FaEye, FaEyeSlash, FaPerson } from "react-icons/fa6";
 import { MdOutlineWork } from "react-icons/md";
 import { TbHomeCancel } from "react-icons/tb";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { setQueryMetric } from "../features/queryMetric/queryMetricSlice";
 import './Visor.scss';
+import { RootState } from "../app/store";
 
 const Visor = ({ metrics }: { metrics: any }) => {
     const dispatch = useDispatch();
     const [isOpen, setIsOpen] = useState(true);
+
+    const queryMetric = useSelector((state: RootState) => state.queryMetric.queryMetric );
 
     const toggleAccordion = () => {
         setIsOpen(!isOpen);
@@ -40,6 +43,11 @@ const Visor = ({ metrics }: { metrics: any }) => {
                     <Box className="stat-row" onClick={() => { configureMetric("POBTOT") }}>
                         <Box className="stat-title-box">
                             <Text className="stat-title">Población</Text>
+                            {
+                                queryMetric == "POBTOT"?
+                                <Icon className="visor__icon--active" as={FaEye}> </Icon> :
+                                <Icon className="visor__icon" as={FaEyeSlash}> </Icon>
+                            }
                         </Box>
                         <Box className="stat-value">
                             <Icon as={FaPerson} />
@@ -49,6 +57,11 @@ const Visor = ({ metrics }: { metrics: any }) => {
                     <Box className="stat-row" onClick={() => { configureMetric("VIVTOT") }}>
                         <Box className="stat-title-box">
                             <Text className="stat-title">Número de Viviendas</Text>
+                            {
+                                queryMetric == "VIVTOT"?
+                                <Icon as={FaEye}> </Icon> :
+                                <Icon as={FaEyeSlash}> </Icon>
+                            }
                         </Box>
                         <Box className="stat-value">
                             <Icon as={BiSolidHome} />
@@ -58,6 +71,11 @@ const Visor = ({ metrics }: { metrics: any }) => {
                     <Box className="stat-row" onClick={() => { configureMetric("VIVPAR_DES") }}>
                         <Box className="stat-title-box">
                             <Text className="stat-title">Viviendas Deshabitadas</Text>
+                            {
+                                queryMetric == "VIVPAR_DES"?
+                                <Icon as={FaEye}> </Icon> :
+                                <Icon as={FaEyeSlash}> </Icon>
+                            }
                         </Box>
                         <Box className="stat-value">
                             <Icon as={TbHomeCancel} />
@@ -67,6 +85,11 @@ const Visor = ({ metrics }: { metrics: any }) => {
                     <Box className="stat-row" onClick={() => { configureMetric("num_establishments") }}>
                         <Box className="stat-title-box">
                             <Text className="stat-title">Número de Establecimientos</Text>
+                            {
+                                queryMetric == "num_establishments"?
+                                <Icon className="visor__icon" as={FaEye}> </Icon> :
+                                <Icon as={FaEyeSlash}> </Icon>
+                            }
                         </Box>
                         <Box className="stat-value">
                             <Icon as={FaBuilding} />
@@ -76,6 +99,11 @@ const Visor = ({ metrics }: { metrics: any }) => {
                     <Box className="stat-row" onClick={() => { configureMetric("num_workers") }}>
                         <Box className="stat-title-box">
                             <Text className="stat-title">Número de Trabajadores</Text>
+                            {
+                                queryMetric == "num_workers"?
+                                <Icon className="active" as={FaEye}> </Icon> :
+                                <Icon as={FaEyeSlash}> </Icon>
+                            }
                         </Box>
                         <Box className="stat-value">
                             <Icon as={MdOutlineWork} />
