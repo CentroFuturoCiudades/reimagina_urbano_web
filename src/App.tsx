96c9c6e903b1c3b2bff225c542d4bdcf3c32c8cc
urielsu96 <uriel.s@tec.mx>
2024-08-19 17:35:58 -0600

    Refactor of all files, deleted unnecesary files, refactor existing code

--- src/App.tsx
+++ src/App.tsx
@@ -1,118 +1,23 @@
-import { Box, Radio, RadioGroup, Stack } from "@chakra-ui/react";
-import { useEffect, useState } from "react";
-import "@chatscope/chat-ui-kit-styles/dist/default/styles.min.css";
-import axios from "axios";
-
-import { Custom3DMap } from "./Custom3DMap";
-import { CustomMap } from "./CustomMap";
-import { API_URL } from "./constants";
-import "./App.css";
-import { Chat, LotSidebar, ConfigurationToolbar, MainSidebar, BaseMap } from "./components";
-import { Icon, IconButton } from "@chakra-ui/react";
-import { MdAdd, MdOutlineMotionPhotosOff } from "react-icons/md";
 import React from "react";
-import { Configuration, GenericObject } from "./types";
-import { LensMap } from "./LensMap";
-import { useFetchGeo } from "./utils";
-//import Legend from "./Legend";
+import "@chatscope/chat-ui-kit-styles/dist/default/styles.min.css";
+
+import "./App.css";
+import { MainSidebar, BaseMap } from "./components";
 
 function App() {
-  const [selectedLots, setSelectedLots] = useState<string[]>([]);
-  const [aggregatedInfo, setAggregatedInfo] = useState<GenericObject>();
-  const [configuration, setConfiguration] = useState<GenericObject>({
-    condition: undefined,
-    metric: "POBTOT",
-    isSatellite: false,
-    visible: {
-      parking: true,
-      building: true,
-      potential_building: true,
-      park: true,
-      green: false,
-      equipment: true,
-    },
-    accessibility_info: {
-      // Initialize this with default values for proximity settings
-      proximity_small_park: 2,
-      proximity_salud: 2,
-      proximity_educacion: 1,
-      proximity_servicios: 5,
-      proximity_supermercado: 1,
-    },
-  });
-  // console.log(selectedLots); // igual a selected Ids
-  const [coords, setCoords] = useState();
-  const [parques, setParques] = useState<Configuration>({
-    activated: true,
-    value: 0,
-  });
-  const [salud, setSalud] = useState({ activated: true, value: 0 });
-  const [educacion, setEducacion] = useState({ activated: true, value: 0 });
-  const [servicios, setServicios] = useState({ activated: true, value: 0 });
-  const [supermercados, setSupermercados] = useState({
-    activated: true,
-    value: 0,
-  });
-  const project = window.location.pathname.split("/")[1];
-  const [mode, setMode] = useState("analysis");
+    const project = window.location.pathname.split("/")[1];
 
-  // if project is undefined, redirect to /primavera
-  if (project === "") {
-    window.location.href = "/primavera";
-  }
+    // if project is undefined, redirect to /primavera
+    if (project === "") {
+        window.location.href = "/primavera";
+    }
 
-  // console.log(aggregatedInfo)
-
-  return (
-    <div style={{ width: "100dvw", height: "100dvh" }}>
-      {/* <Chat
-        onSend={(query) =>
-          setConfiguration({
-            ...configuration,
-            metric: query.metric,
-            condition: query.condition,
-          })
-        }
-      /> */}
-      <MainSidebar>
-      </MainSidebar>
-      <BaseMap></BaseMap>
-      {/* {mode === "explore" ? (
-        <LensMap
-          aggregatedInfo={aggregatedInfo}
-          data={data}
-          selectedLots={selectedLots}
-          setSelectedLots={setSelectedLots}
-          visible={configuration.visible}
-          coords={coords}
-          metric={configuration.metric}
-          isSatellite={configuration.isSatellite}
-        />
-      ) : (
-        <CustomMap
-          aggregatedInfo={aggregatedInfo}
-          data={data}
-          selectedLots={selectedLots}
-          setSelectedLots={setSelectedLots}
-          visible={configuration.visible}
-          coords={coords}
-          metric={configuration.metric}
-          isSatellite={configuration.isSatellite}
-        />
-      )} */}
-      {/* <ConfigurationToolbar
-        configuration={configuration}
-        setConfiguration={setConfiguration}
-        supermercados={supermercados}
-        setParques={setParques}
-        setSalud={setSalud}
-        setEducacion={setEducacion}
-        setServicios={setServicios}
-        setSupermercados={setSupermercados}
-      /> */}
-
-    </div>
-  );
+    return (
+        <div style={{ width: "100dvw", height: "100dvh" }}>
+            <MainSidebar />
+            <BaseMap />
+        </div>
+    );
 }
 
 export default App;
--- src/components/Legend/Legend.tsx
+++ src/components/Legend/Legend.tsx
@@ -1,16 +1,18 @@
-import React, { useMemo } from 'react';
-import * as d3 from 'd3';
+import React from "react";
+import * as d3 from "d3";
 
 import "./Legend.scss";
-import { useSelector } from 'react-redux';
-import { RootState } from '../../app/store';
-import { COLUMN_MAPPING, VIEW_COLORS_RGBA } from '../../constants';
+import { useSelector } from "react-redux";
+import { RootState } from "../../app/store";
+import { COLUMN_MAPPING, VIEW_COLORS_RGBA } from "../../constants";
 
-const Legend = ( ) => {
-
-
-    const queryData = useSelector((state: RootState) => state.queryData.queryData);
-    const metric = useSelector((state: RootState) => state.queryMetric.queryMetric);
+const Legend = () => {
+    const queryData = useSelector(
+        (state: RootState) => state.queryData.queryData
+    );
+    const metric = useSelector(
+        (state: RootState) => state.queryMetric.queryMetric
+    );
 
     const domain = [
         Math.min(...(Object.values(queryData) as any)),
@@ -25,45 +27,54 @@ const Legend = ( ) => {
         8
     );
 
-    const quantiles = d3
-                    .scaleQuantize<string>()
-                    .domain(domain)
-                    .range(colors);
+    const quantiles = d3.scaleQuantize<string>().domain(domain).range(colors);
 
+    const title = COLUMN_MAPPING[metric] || metric;
 
-    const title = COLUMN_MAPPING[ metric ] || metric;
-
-    const formatValue = ( value: string ) => {
-        const formattedValue = Number(value).toLocaleString('en-US');
-        if (title.toLowerCase().includes('porcentaje') || title.toLowerCase().includes('ratio')) {
-        return `${formattedValue}%`;
-        } else if (title.toLowerCase().includes('minutos') || title.toLowerCase().includes('minutes')) {
-        return `${formattedValue} min`;
-        } else if (title.toLowerCase().includes('área') || title.toLowerCase().includes('area')) {
-        return `${formattedValue} m²`;
+    const formatValue = (value: string) => {
+        const formattedValue = Number(value).toLocaleString("en-US");
+        if (
+            title.toLowerCase().includes("porcentaje") ||
+            title.toLowerCase().includes("ratio")
+        ) {
+            return `${formattedValue}%`;
+        } else if (
+            title.toLowerCase().includes("minutos") ||
+            title.toLowerCase().includes("minutes")
+        ) {
+            return `${formattedValue} min`;
+        } else if (
+            title.toLowerCase().includes("área") ||
+            title.toLowerCase().includes("area")
+        ) {
+            return `${formattedValue} m²`;
         }
         return formattedValue;
     };
 
-    if( !Object.keys( queryData ).length ){
-        return <></>
+    if (!Object.keys(queryData).length) {
+        return <></>;
     }
 
     return (
-        <div className='legend' >
-        <h4 className='legend__title'>
-            <b>{title}</b>
-        </h4>
-        {
-            colors.map((color, i) => (
-            <div key={i} className='legend__label'>
-                <div className='legend__icon' style={{ background: color }} />
-                <span>
-                {quantiles.invertExtent(color).map( (d) => formatValue(d.toFixed(2))).join(' - ')}
-                </span>
-            </div>
-            ))
-        }
+        <div className="legend">
+            <h4 className="legend__title">
+                <b>{title}</b>
+            </h4>
+            {colors.map((color, i) => (
+                <div key={i} className="legend__label">
+                    <div
+                        className="legend__icon"
+                        style={{ background: color }}
+                    />
+                    <span>
+                        {quantiles
+                            .invertExtent(color)
+                            .map((d) => formatValue(d.toFixed(2)))
+                            .join(" - ")}
+                    </span>
+                </div>
+            ))}
         </div>
     );
 };
